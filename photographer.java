4686ff80919955c9f128255e88e47391dd696455
annatresasiji <143536952+annatresasiji@users.noreply.github.com>
2024-07-04 23:05:08 +0530

    Create photographer.java

---
+++ photographer.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EventManagement_system;
+
+public class Photographer  {
+   
+}
